f266dd8cb623fb9cd618b64d8faefa4a2cf48394
Navaneeth V <navaneethvinod18@gmail.com>
2024-02-17 16:53:37 +0530

    traversal and inserstion done

--- array/elementary.js
+++ array/elementary.js
@@ -1,9 +1,22 @@
 // Array Traversal
 
-function myArray(arr){
-    for(i=0;i<arr.length-;i++){
-        console.log(arr[i]);
-    }
-}
+// function myArray(arr){
+//     for(let i=0;i<arr.length-;i++){
+//         console.log(arr[i]);
+//     }
+// }
 
-// myArray([2,3,4,5])
+// Array Insertion
+
+// function myArray(arr){
+//     let elem = 10
+//     let pos = 2
+//     for(let i=arr.length-1;i>=pos;i--){
+//         arr[i]=arr[i-1]
+//     }
+//     arr[pos-1] = elem
+//     console.log(arr);
+// }   
+
+
+
