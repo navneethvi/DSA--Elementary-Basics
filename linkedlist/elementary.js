fcc8c221f0fd41958b2af6735f02c9f520e6d0fd
Navaneeth V <navaneethvinod18@gmail.com>
2024-02-19 14:58:17 +0530

    linkedlist

--- linkedlist/elementary.js
+++ linkedlist/elementary.js
@@ -67,5 +67,58 @@
 
 // console.log(list.isEmpty());
 // console.log(list.getSize());
-
 // list.prepend(10)
+
+
+// LinkedList Print
+
+// class Node {
+//     constructor(value){
+//         this.value = value
+//         this.next = null
+//     }
+// }
+
+// class LinkedList {
+//     constructor(){
+//         this.head = null
+//         this.size = 0
+//     }
+
+//     getSize(){
+//         return this.size
+//     }
+
+//     prepend(value){
+//         const node = new Node(value)
+//         if(this.size === 0){
+//             this.head = node
+//         }else{
+//             node.next = this.head
+//             this.head = node
+//         }
+//         this.size ++
+//     }
+
+//     print(){
+//         if(this.size === 0){
+//             console.log("Linkedlist is empty");
+//         }else{
+//             let listValues = ""
+//             let curr = this.head
+//             while(curr){
+//                 listValues += `${curr.value} `
+//                 curr = curr.next
+//             }
+//             console.log(listValues);
+//         }
+//     }
+// }
+
+// const list = new LinkedList()
+// list.prepend(20)
+// list.prepend(10)
+// list.prepend(30)
+// list.prepend(40)
+// console.log(list.getSize());
+// list.print()
