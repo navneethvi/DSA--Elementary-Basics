dd148a7efb4fd716563dfb0ac5215f63d5c9a4a8
Navaneeth V <navaneethvinod18@gmail.com>
2024-02-19 20:42:59 +0530

    linkedlist

--- linkedlist/elementary.js
+++ linkedlist/elementary.js
@@ -122,3 +122,72 @@
 // list.prepend(40)
 // console.log(list.getSize());
 // list.print()
+
+// LinkedList Append
+
+// class Node {
+//     constructor(value) {
+//         this.value = value
+//         this.next = null
+//     }
+// }
+
+// class LinkedList {
+//     constructor() {
+//         this.head = null
+//         this.size = 0
+//     }
+
+//     getSize() {
+//         return this.size
+//     }
+
+//     prepend(value) {
+//         const node = new Node(value)
+//         if (this.getSize() === 0) {
+//             this.head = node
+//         } else {
+//             node.next = this.head
+//             this.head = node
+//         }
+//         this.size++
+//     }
+
+//     append(value) {
+//         const node = new Node(value)
+//         if (this.getSize() === 0) {
+//             this.head = node
+//         } else {
+//             let prev = this.head
+//             while (prev.next) {
+//                 prev = prev.next
+//             }
+//             prev.next = node
+//         }
+//         this.size ++
+//     }
+
+
+//     print() {
+//         if (this.getSize() === 0) {
+//             console.log("linkedlist is empty");
+//         } else {
+//             let curr = this.head
+//             let listValues = ""
+//             while (curr) {
+//                 listValues += `${curr.value} `
+//                 curr = curr.next
+//             }
+//             console.log(listValues);
+//         }
+//     }
+// }
+
+// const list = new LinkedList()
+// list.print()
+// list.prepend(10)
+// list.prepend(20)
+// list.prepend(30)
+// list.print()
+// list.append(500)
+// list.print()
