25694dd27540df0423df43aa93206c148fc624ae
Navaneeth V <navaneethvinod18@gmail.com>
2024-02-19 10:34:26 +0530

    linkedlist

--- linkedlist/elementary.js
+++ linkedlist/elementary.js
@@ -22,8 +22,50 @@
 //     }
 // }
 
-// const list = new LinkedList
+// const list = new LinkedList()
 
 // console.log(list.isEmpty());
 // console.log(list.getSize());
 
+
+// LinkedList Prepend
+
+// class Node {
+//     constructor(value){
+//         this.value = value 
+//         this.next = null
+//     }
+// }
+
+// class LinkedList {
+//     constructor(){
+//         this.head = null
+//         this.size = 0
+//     }
+
+//     isEmpty(){
+//         return this.size === 0
+//     }
+
+//     getSize(){
+//         return this.size
+//     }
+
+//     prepend(value){
+//         const node = new Node()
+//         if(this.isEmpty){
+//             this.head = node
+//         }else{
+//             node.next = this.head
+//             this.head = node
+//         }
+//         this.size ++
+//     }
+// }
+
+// const list = new LinkedList()
+
+// console.log(list.isEmpty());
+// console.log(list.getSize());
+
+// list.prepend(10)
