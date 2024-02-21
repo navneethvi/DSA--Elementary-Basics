185760250d983fd3a060b318aedf72998c66b961
Navaneeth V <navaneethvinod18@gmail.com>
2024-02-22 03:16:17 +0530

    search

--- search/binary.js
+++ search/binary.js
@@ -1,5 +1,5 @@
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-let target = 2
+let target = 6
 
 function binarySearch(arr, data) {
     let left = 0
@@ -7,7 +7,7 @@ function binarySearch(arr, data) {
 
     while (left <= right) {
         let mid = Math.floor((left + right) / 2)
-        
+
         if (arr[mid] === data) {
             return mid
         } else if (arr[mid] < data) {
@@ -19,4 +19,19 @@ function binarySearch(arr, data) {
     return -1
 }
 
-console.log(binarySearch(arr, target));
+function binarySearchRecursive(arr, data, left = 0, right = arr.length - 1){
+    if(left>right){
+        return -1
+    }
+    let mid = Math.floor((left+right)/2)
+    if(arr[mid]===data){
+        return mid
+    }else if(arr[mid]<data){
+        return binarySearchRecursive(arr, data, mid + 1, right)
+    }else{
+        return binarySearchRecursive(arr, data, left, right - 1)
+    }
+}
+
+// console.log(binarySearch(arr, target));
+// console.log(binarySearchRecursive(arr, target));
