556c35781d4eaea896d9bdfba7d55d04b5f8304c
Navaneeth V <navaneethvinod18@gmail.com>
2024-02-25 18:06:35 +0530

    code optimised

--- playground/play.js
+++ playground/play.js
@@ -1,17 +1,17 @@
-function binarySearch(arr, value, left = 0, right = arr.length-1){
-    if(left>right){
-        return -1
-    }
-    let mid = Math.floor((left+right)/2)
+// function binarySearch(arr, value, left = 0, right = arr.length-1){
+//     if(left>right){
+//         return -1
+//     }
+//     let mid = Math.floor((left+right)/2)
 
-    if(arr[mid]===value){
-        return mid
-    }else if(arr[mid]<value){
-        return binarySearch(arr, value, left+1, right)
-    }else{
-        return binarySearch(arr, value, left, right-1)
-    }
-}
+//     if(arr[mid]===value){
+//         return mid
+//     }else if(arr[mid]<value){
+//         return binarySearch(arr, value, left+1, right)
+//     }else{
+//         return binarySearch(arr, value, left, right-1)
+//     }
+// }
 
-// console.log(binarySearch([1,2,3,4,5,6], 1));
+// // console.log(binarySearch([1,2,3,4,5,6], 1));
 
