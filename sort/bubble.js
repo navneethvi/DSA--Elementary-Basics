25c0d14472431ab8ca958db84bd6d3d133c8d504
Navaneeth V <navaneethvinod18@gmail.com>
2024-02-29 20:53:11 +0530

    code optimised

--- sort/bubble.js
+++ sort/bubble.js
@@ -1,13 +1,18 @@
-const arr = [6,5,4,3,2,1]
+const arr = [10, 5, 4, 3, 2, 1]
 
-function bubbleSort(arr){
-    for(let i=0;i<arr.length-1;i++){
-        for(let j=0;j<arr.length-1-i;j++){
-            if(arr[j] > arr[i]+1){
-               
+function bubbleSort(arr) {
+    let swapped
+    do {
+        swapped = false
+        for (i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i]
+                arr[i] = arr[i + 1]
+                arr[i + 1] = temp
+                swapped = true
             }
         }
-    }
+    } while (swapped)
     return arr
 }
 
