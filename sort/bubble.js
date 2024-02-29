9bdf0dd606545dde33aac48ca3714297fbd1812d
Navaneeth V <navaneethvinod18@gmail.com>
2024-02-29 13:18:46 +0530

    code optimised

---
+++ sort/bubble.js
@@ -0,0 +1,14 @@
+const arr = [6,5,4,3,2,1]
+
+function bubbleSort(arr){
+    for(let i=0;i<arr.length-1;i++){
+        for(let j=0;j<arr.length-1-i;j++){
+            if(arr[j] > arr[i]+1){
+               
+            }
+        }
+    }
+    return arr
+}
+
+console.log(bubbleSort(arr));
