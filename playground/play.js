63b2efdbecbef64b9c1a9177328ecc6c402f01d9
Navaneeth V <navaneethvinod18@gmail.com>
2024-03-01 09:56:28 +0530

    code optimised

--- playground/play.js
+++ playground/play.js
@@ -15,22 +15,22 @@
 
 // console.log(binarySearch([1,2,3,4,5,6], 1));
 
-// let arr =  [5,4,3,2,1,0]
+let arr = [5, 4, 3, 2, 1, 0]
 
-// function bubbleSort(arr){
-//     let swapped
-//     do {
-//         swapped = false
-//         for(i=0;i<arr.length-1;i++){
-//             if(arr[i]>arr[i+1]){
-//                 let temp = arr[i]
-//                 arr[i] = arr[i+1]
-//                 arr[i+1] = temp
-//                 swapped = true
-//             }
-//         }
-//     } while (swapped)
-//     return arr
-// }
+function bubbleSort(arr) {
+    let swapped
+    do {
+        swapped = false
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i]
+                arr[i] = arr[i + 1]
+                arr[i + 1] = temp
+                swapped = true
+            }
+        }
+    } while (swapped)
+    return arr
+}
 
-// console.log(bubbleSort(arr));
+console.log(bubbleSort(arr));
