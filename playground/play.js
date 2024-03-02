4a273136b4fcbbcbb975b1ce1a132ebeb89905b8
Navaneeth V <navaneethvinod18@gmail.com>
2024-03-02 11:05:34 +0530

    code optimised

--- playground/play.js
+++ playground/play.js
@@ -36,20 +36,20 @@ let arr = [5, 4, 3, 2, 1, 0]
 
 
 
-// function insertionSort(arr) {
-//     for (let i = 1; i < arr.length; i++) {
-//         let temp = arr[i]
-//         j = i - 1
-//         while (j >= 0 && arr[j] > temp) {
-//             arr[j + 1] = arr[j]
-//             j = j - 1
-//         }
-//         arr[j+1] = temp
-//     }
-//     return arr
-// }
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let temp = arr[i]
+        j = i - 1
+        while (j >= 0 && arr[j] > temp) {
+            arr[j + 1] = arr[j]
+            j = j - 1
+        }
+        arr[j+1] = temp
+    }
+    return arr
+}
 
-// console.log(insertionSort(arr));
+console.log(insertionSort(arr));
 
 
 // function bubbleSort(arr) {
