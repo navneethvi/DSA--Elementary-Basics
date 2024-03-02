bbeedc59022d1de411977b590c52806fec8edb34
Navaneeth V <navaneethvinod18@gmail.com>
2024-03-02 10:27:28 +0530

    code optimised

--- playground/play.js
+++ playground/play.js
@@ -152,5 +152,21 @@ let arr = [5, 4, 3, 2, 1, 0]
 //     return arr
 // }
 
-
 // console.log(insertionSort(arr));
+
+
+// function insertionSort(arr){
+//     for(i=1;i<arr.length;i++){
+//         let temp = arr[i]
+//         let j = i - 1
+//         while(j>=0&&arr[j]>temp){
+//             arr[j+1] = arr[j]
+//             j--
+//         }
+//         arr[j+1] = temp
+//     }
+//     return arr
+// }
+
+
+// console.log(insertionSort([10,2,33,54,23,65,43,24]));
