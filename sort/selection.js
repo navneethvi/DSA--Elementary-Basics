3fb276ec92607e9dc36d59203dff3178a3168496
Navaneeth V <navaneethvinod18@gmail.com>
2024-03-03 21:19:50 +0530

    code optimised

--- sort/selection.js
+++ sort/selection.js
@@ -1,10 +1,9 @@
-function selectionSort(arr){
-    for(i=0;i<arr.length;i++){
-        let minIndex = i
-        for(j=1;j<arr.length;j++){
-            if(arr[j]<arr[minIndex]){
-                minIndex = j
-            }
-        }
-    }
-}
+// function selectionSort(arr){
+//     for(let i=0;i<arr.length-1;i++){
+        
+//     }
+// }
+
+// let arr = [3, 2, 5, 6, 1, 9, 7]
+
+// console.log(selectionSort(arr));0
