5f09c0968a1cf0f331f5dfd2ed4db37ac76df90b
Navaneeth V <navaneethvinod18@gmail.com>
2024-03-04 12:07:57 +0530

    queue

--- queue/elementary.js
+++ queue/elementary.js
@@ -1,13 +1,35 @@
 class Queue {
-    constructor(){
-        this.items = []
+    constructor() {
+        this.dataStore = []
+        this.head = 0
+        this.tail = 0
     }
 
-    enqueue(value){
-        this.items.push(value)
+    enqueue(value) {
+        this.dataStore[this.tail++] = value
     }
 
-    dequeue(value){
-        this.items.shift()
+    dequeue() {
+        return this.dataStore[this.head++]
     }
+
+    peek() {
+        return this.dataStore[this.head]
+    }
+
+    size() {
+        return this.tail - this.head
+    }
+
+    print(){
+        for(let i=this.head;i<this.tail;i++){
+            console.log(this.dataStore[i]);
+        }
+    }
+
 }
+
+const queue = new Queue()
+queue.enqueue(20)
+queue.enqueue(30)
+queue.print()
