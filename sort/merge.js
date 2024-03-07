c6a726891ac036237910000885e8b4ea3802b1fe
Navaneeth V <navaneethvinod18@gmail.com>
2024-03-08 02:25:00 +0530

    merge

---
+++ sort/merge.js
@@ -0,0 +1,26 @@
+function mergeSort(arr){
+    if(arr.length<=1){
+        return arr
+    }
+
+    let mid = Math.floor(arr.length/2)
+    let left = arr.slice(0,mid)
+    let right = arr.slice(mid)
+    return merge(mergeSort(left), mergeSort(right))
+}
+
+function merge(left, right){
+    let result = []
+    while(left.length&&right.length){
+        if(left[0]<right[0]){
+            result.push(left.shift())
+        }else{
+            result.push(right.shift())
+        }
+    }
+    return result.concat(left, right);}
+
+const array = [8, 3, 5, 1, 9, 2, 7, 4, 6];
+console.log("Original array:", array);
+const sortedArray = mergeSort(array);
+console.log("Sorted array:", sortedArray);
