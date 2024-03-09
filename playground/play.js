12805852a2032ef2ad1caa7a3871a4fc64a0d6cb
Navaneeth V <navaneethvinod18@gmail.com>
2024-03-09 18:32:44 +0530

    code optimised

--- playground/play.js
+++ playground/play.js
@@ -1,3 +1,5 @@
+const { removeAllListeners } = require("nodemon")
+
 let arr = [5, 4, 3, 2, 1, 0]
 
 function binarySearch(arr, value, left = 0, right = arr.length - 1) {
@@ -581,6 +583,166 @@ function binarySearch(arr, value, left = 0, right = arr.length - 1) {
 
 
 
+// function bubbleSort(arr) {
+//     let swapped
+//     do {
+//         swapped = false
+//         for (let i = 0; i < arr.length; i++) {
+//             if (arr[i] > arr[i + 1]) {
+//                 let temp = arr[i]
+//                 arr[i] = arr[i + 1]
+//                 arr[i + 1] = temp
+//                 swapped = true
+//             }
+//         }
+//     } while (swapped);
+//     return arr
+// }
+
+// function insertionSort(arr) {
+//     for (let i = 1; i < arr.length; i++) {
+//         let temp = arr[i]
+//         let j = i - 1
+//         while (j >= 0 && arr[j] > temp) {
+//             arr[j + 1] = arr[j]
+//             j--
+//         }
+//         arr[j + 1] = temp
+//     }
+//     return arr
+// }
+
+// function selectionSort(arr) {
+//     for (let i = 0; i < arr.length - 1; i++) {
+//         let min = i
+//         for (let j = i + 1; j < arr.length; j++) {
+//             if (arr[j] < arr[min]) {
+//                 min = j
+//             }
+//         }
+//         let temp = arr[min]
+//         arr[min] = arr[i]
+//         arr[i] = temp
+//     }
+//     return arr
+// }
+
+
+
+
+// function insertionSort(arr) {
+//     for (i = 0; i < arr.length; i++) {
+//         let temp = arr[i]
+//         let j = i - 1
+//         while (j >= 0 && arr[j] > temp) {
+//             arr[j + 1] = arr[j]
+//             j--
+//         }
+//         arr[j + 1] = temp
+//     }
+//     return arr
+// }
+
+// function selectionSort(arr) {
+//     for (let i = 0; i < arr.length - 1; i++) {
+//         let min = i
+//         for (let j = i + 1; j < arr.length; j++) {
+//             if (arr[j] < arr[min]) {
+//                 min = j
+//             }
+//         }
+//         [arr[i], arr[min]] = [arr[min], arr[i]]
+//     }
+//     return arr
+// }
+
+
+// function partition(arr, start, end){
+//     let pivot = arr[end]
+//     let i = start - 1
+//     for(let j=start;j<end;j++){
+//         if(arr[j]<pivot){
+//             i++
+//             swap(arr,i,j)
+//         }
+//     }
+//     swap(arr,i+1,end)
+//     return i + 1
+// }
+
+// function swap(arr, i,j){
+//     let temp = arr[i]
+//     arr[i] = arr[j]
+//     arr[j] = temp
+// }
+
+// function quickSort(arr, start = 0, end = arr.length - 1){
+//     if(start<end){
+//         const pivotIndex = partition(arr,start,end)
+//         quickSort(arr,start,pivotIndex-1)
+//         quickSort(arr,pivotIndex+1, end)
+
+//     }
+//     return arr
+// }
+
+
+
+
+// function mergeSort(arr){
+//     if(arr.length<=1){
+//         return arr
+//     }
+
+//     let mid = Math.floor(arr.length/2)
+//     let left = arr.slice(0,mid)
+//     let right = arr.slice(mid)
+//     return merge(mergeSort(left), mergeSort(right))
+// }
+
+// function merge(left, right){
+//     let result = []
+//     while(left.length&&right.length){
+//         if(left[0]<right[0]){
+//             result.push(left.shift())
+//         }else{
+//             result.push(right.shift())
+//         }
+//     }
+//     return result.concat(left, right)
+// }
+
+
+// function bubbleSort(arr) {
+//     let swapped
+//     do {
+//         swapped = false
+//         for (let i = 0; i < arr.length; i++) {
+//             if (arr[i] > arr[i + 1]) {
+//                 let temp = arr[i]
+//                 arr[i] = arr[i + 1]
+//                 arr[i + 1] = temp
+//                 swapped = true
+//             }
+//         }
+//     } while (swapped);
+//     return arr
+// }
+
+// function insertionSort(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         let temp = arr[i]
+//         let j = i - 1
+//         while(j>=0&&arr[j]>temp){
+//             arr[j+1] = arr[j]
+//             j--
+//         }
+//         arr[j+1] = temp
+//     }
+//     return arr
+// }
+
+
 function bubbleSort(arr) {
     let swapped
     do {
@@ -598,7 +760,7 @@ function bubbleSort(arr) {
 }
 
 function insertionSort(arr) {
-    for (let i = 1; i < arr.length; i++) {
+    for (i = 1; i < arr.length; i++) {
         let temp = arr[i]
         let j = i - 1
         while (j >= 0 && arr[j] > temp) {
@@ -610,38 +772,25 @@ function insertionSort(arr) {
     return arr
 }
 
+
 function selectionSort(arr) {
-  for(let i=0;i<arr.length-1;i++){
-    let min = i
-    for(let j=i+1;j<arr.length;j++){
-        if(arr[j]<arr[min]){
-            min = j
+    for (i = 0; i < arr.length - 1; i++) {
+        let min = i
+        for(let j=i+1;j<arr.length;j++){
+            if(arr[j]<arr[min]){
+                min = j
+            }
         }
-    }
-    let temp = arr[min]
-    arr[min] = arr[i]
-    arr[i] = temp
-  }
-  return arr
-}
-
-
-
-function insertionSort(arr){
-    for(i=0;i<arr.length;i++){
-        let temp = arr[i]
-        let j = i - 1
-        while(j>=0&&arr[j]>temp){
-            arr[j+1] = arr[j]
-            j--
-        }
-        arr[j+1] = temp
+        [arr[min], arr[i]] = [arr[i], arr[min]]
     }
     return arr
 }
 
-// console.log(bubbleSort([3, 2, 5, 6, 8, 9, 11, 2, 4]))
+
+
+
+console.log(bubbleSort([3, 2, 5, 6, 8, 9, 11, 2, 4]))
 console.log(insertionSort([3, 2, 5, 6, 8, 9, 11, 2, 4]))
-// console.log(selectionSort([3, 2, 5, 6, 8, 9, 11, 2, 4]));
+console.log(selectionSort([3, 2, 5, 6, 8, 9, 11, 2, 4]));
 // console.log(quickSort([3, 2, 5, 6, 8, 9, 11, 2, 4]));
 // console.log(mergeSort([3, 2, 5, 6, 8, 9, 11, 2, 4]));
