e2619eaf23c4af5b16e12c5ef804536eff577354
Navaneeth V <navaneethvinod18@gmail.com>
2024-03-10 18:27:55 +0530

    code optimised

--- playground/play.js
+++ playground/play.js
@@ -331,6 +331,133 @@ function binarySearch(arr, value, left = 0, right = arr.length - 1) {
 // }
 
 
+
+// class Node {
+//     constructor(value){
+//         this.data = value
+//         this.next = null
+//     }
+// }
+
+// class Stack {
+//     constructor(){
+//         this.top = null
+//         this.size = 0
+//     }
+
+//     push(value){
+//         const newNode = new Node(value)
+//         if(this.size===0){
+//             this.top = newNode
+//         }else{
+//             newNode.next = this.top
+//             this.top = newNode
+//         }
+//         this.size++
+//     }
+
+//     pop(){
+//         if(this.top===null){
+//             console.log("Stack empty");
+//         }else{
+//             this.top = this.top.next
+//             this.size --
+//         }
+//     }
+
+//     peek(){
+//         if(this.size!==0){
+//             console.log(this.top.data);
+//         }else{
+//             console.log("Stack empty");
+//         }
+//     }
+
+//     print(){
+//         if(this.top === null){
+//             console.log("Stack empty");
+//         }else{
+//             let curr = this.top
+//             let elems = ""
+//             while(curr){
+//                 elems+=curr.data+ " "
+//                 curr = curr.next
+//             }
+//             console.log(elems);
+//         }
+//     }
+// }
+
+
+// const stack = new Stack()
+// // stack.pop()
+// stack.push(10)
+// stack.push(20)
+// stack.push(30)
+// stack.print()
+// stack.pop()
+// stack.print()
+// stack.peek()
+
+
+// class Node {
+//     constructor(value){
+//         this.data = value
+//         this.next = null
+//     }
+// }
+
+// class Queue {
+//     constructor(){
+//         this.front = null
+//         this.rear = null
+//         this.size = 0
+//     }
+
+//     enque(value){
+//         const newNode = new Node(value)
+//         if(this.front === null){
+//             this.front = newNode
+//             this.rear = newNode
+//         }else{
+//             this.rear.next = newNode
+//             this.rear = newNode
+//         }
+//         this.size++
+//     }
+
+//     deque(){
+//         if(this.front===null){
+//             console.log("Queue is empty");
+//         }else{
+//             this.front = this.front.next
+//             this.size--
+//         }
+//     }
+
+//     print(){
+//         if(this.size === 0){
+//             console.log("Queue is empty");
+//         }else{
+//             let elems = ""
+//             let curr = this.front
+//             while(curr){
+//                 elems += curr.data+" "
+//                 curr = curr.next
+//             }
+//             console.log(elems);
+//         }
+//     }
+// }
+
+// const queue = new Queue()
+// queue.enque(20)
+// queue.enque(10)
+// queue.enque(1)
+// queue.print()
+// queue.deque()
+// queue.print()
+
 // function bubbleSort(arr) {
 //     let swapped
 //     do {
@@ -743,15 +870,57 @@ function binarySearch(arr, value, left = 0, right = arr.length - 1) {
 // }
 
 
+// function bubbleSort(arr) {
+//     let swapped
+//     do {
+//         swapped = false
+//         for (let i = 0; i < arr.length; i++) {
+//             if (arr[i] > arr[i + 1]) {
+//                 let temp = arr[i]
+//                 arr[i] = arr[i + 1]
+//                 arr[i + 1] = temp
+//                 swapped = true
+//             }
+//         }
+//     } while (swapped);
+//     return arr
+// }
+
+// function insertionSort(arr) {
+//     for (i = 1; i < arr.length; i++) {
+//         let temp = arr[i]
+//         let j = i - 1
+//         while (j >= 0 && arr[j] > temp) {
+//             arr[j + 1] = arr[j]
+//             j--
+//         }
+//         arr[j + 1] = temp
+//     }
+//     return arr
+// }
+
+
+// function selectionSort(arr) {
+//     for (i = 0; i < arr.length - 1; i++) {
+//         let min = i
+//         for(let j=i+1;j<arr.length;j++){
+//             if(arr[j]<arr[min]){
+//                 min = j
+//             }
+//         }
+//         [arr[min], arr[i]] = [arr[i], arr[min]]
+//     }
+//     return arr
+// }
+
+
 function bubbleSort(arr) {
     let swapped
     do {
         swapped = false
         for (let i = 0; i < arr.length; i++) {
             if (arr[i] > arr[i + 1]) {
-                let temp = arr[i]
-                arr[i] = arr[i + 1]
-                arr[i + 1] = temp
+                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]
                 swapped = true
             }
         }
@@ -759,29 +928,29 @@ function bubbleSort(arr) {
     return arr
 }
 
-function insertionSort(arr) {
-    for (i = 1; i < arr.length; i++) {
+
+function insertionSort(arr){
+    for(let i=1;i<arr.length;i++){
         let temp = arr[i]
         let j = i - 1
-        while (j >= 0 && arr[j] > temp) {
-            arr[j + 1] = arr[j]
+        while(j>=0&&arr[j]>temp){
+            arr[j+1] = arr[j]
             j--
         }
-        arr[j + 1] = temp
+        arr[j+1] = temp
     }
     return arr
 }
 
-
-function selectionSort(arr) {
-    for (i = 0; i < arr.length - 1; i++) {
+function selectionSort(arr){
+    for(let i=0;i<arr.length-1;i++){
         let min = i
         for(let j=i+1;j<arr.length;j++){
             if(arr[j]<arr[min]){
                 min = j
             }
         }
-        [arr[min], arr[i]] = [arr[i], arr[min]]
+        [arr[min],arr[i]] = [arr[i],arr[min]]
     }
     return arr
 }
