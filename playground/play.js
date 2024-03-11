1095b6b9351be8d2b465634a5d36ed9edc2f0154
Navaneeth V <navaneethvinod18@gmail.com>
2024-03-11 10:11:22 +0530

    code optimised

--- playground/play.js
+++ playground/play.js
@@ -520,46 +520,43 @@ function binarySearch(arr, value, left = 0, right = arr.length - 1) {
 
 
 
-class HashTable {
-    constructor(){
-        this.table = new Array(size)
-        this.size = size
-    }
+// class HashTable {
+//     constructor(size = 50) {
+//         this.table = new Array(size)
+//         this.size = size
+//     }
 
-    _hash(key){
-        let total = 0
-        for(let i=0;i<key.length;i++){
-            total+=key.charCodeAt(i)
-        }
-        return total%this.size
-    }
+//     hash(key) {
+//         let total = 0
+//         for (let i = 0; i < arr.length; i++) {
+//             total += key.charCodeAt(i)
+//         }
+//         return total % this.size
+//     }
 
-    set(key, value){
-        const index = this._hash(key)
-        this.table[index] = value
-    }
+//     set(key, value) {
+//         const index = this.hash(key)
+//         this.table[index] = value
+//     }
 
-    get(key){
-        const index = this._hash(key)
-        return this.table[index]
-    }
+//     get(key) {
+//         const index = this.hash(key)
+//         return this.table[index]
+//     }
 
-    remove(key){
-        const index = this._hash(key)
-        this.table[index] = undefined
-    }
+//     remove(key) {
+//         const index = this.hash(key)
+//         this.table[index] = undefined
+//     }
+// }
 
-    print(){
-        for(let i=0;i<this.table.length;i++){
-            if(this.table[i]){
-                console.log(i,this.table[i]);
-            }
-        }
-    }
-}
+// const ht = new HashTable()
+// ht.set("student", "navaneeth")
+// ht.set("age", "21")
+// console.log(ht.get("student"));
 
-const HashTable = new HashTable()
-HashTable.set()
+
+
 
 
 // function bubbleSort(arr) {
