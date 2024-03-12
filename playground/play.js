c705f075c9a2bd9131210cb632bac07cd351fff3
Navaneeth V <navaneethvinod18@gmail.com>
2024-03-13 00:40:43 +0530

    code optimised

--- playground/play.js
+++ playground/play.js
@@ -519,6 +519,75 @@
 // queue.print()
 
 
+class Node {
+    constructor(value){
+        this.data = value
+        this.next = null
+    }
+}
+
+class Stack {
+    constructor(){
+        this.top = null
+        this.size = 0
+    }
+
+    push(value){
+        const newNode = new Node(value)
+        if(this.top === null){
+            this.top = newNode
+        }else{
+            newNode.next = this.top
+            this.top = newNode
+        }
+    }
+
+    pushArrayReview(arr){
+        for(let i=0;i<arr.length;i++){
+            this.push(arr[i])
+            this.push(arr[arr.length-1-i])
+        }
+    }
+
+    pushArrayReverse(arr){
+        for(let i=0;i<arr.length;i++){
+            this.push(arr[i])
+        }
+    }
+
+    pop(){
+        if(this.top === null){
+            console.log("Stack underflow");
+        }else{
+            this.top = this.top.next
+            this.size--
+        }
+    }
+
+    print(){
+        if(this.top===null){
+            console.log("Stack empty");
+        }else{
+            let curr = this.top
+            let elems = ""
+            while(curr){
+                elems+= curr.data+ " "
+                curr = curr.next
+            }
+            console.log(elems);
+        }
+    }
+}
+
+const stack = new Stack()
+// stack.pushArray([1,2,3,4,5,6,7,8,9])
+stack.pushArrayReverse([1,2,3,4,5,6])
+stack.print()
+
+stack.print()
+
+
+
 
 // class HashTable {
 //     constructor(size = 50) {
