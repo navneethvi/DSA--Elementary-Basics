6a42be0dc98ca76c7b4ff3aa378b82e4e9b7599e
Navaneeth V <navaneethvinod18@gmail.com>
2024-03-15 05:13:41 +0530

    queue

--- queue/elementary.js
+++ queue/elementary.js
@@ -28,21 +28,65 @@ class Queue {
             console.log("Queue is empty");
         } else {
             this.front = this.front.next
-            if(this.front===null){
+            if (this.front === null) {
                 this.rear = null
             }
             this.size--
         }
     }
 
-    print(){
-        if(this.front===null){
+
+    enqueFormat(arr) {
+        for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+            this.enqueue(arr[i])
+            this.enqueue(arr[arr.length - 1 - i])
+        }
+    }
+
+
+    reverse() {
+        if (this.front === null || this.front.next === null) {
+            return
+        }
+        let prev = null
+        let curr = this.front
+        while (curr) {
+            let next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        }
+        this.rear = this.front
+        this.front = prev
+    }
+
+
+    sort(){
+        let swapped
+        do {
+            swapped = false
+            let curr = this.front
+            while(curr&&curr.next){
+              if(curr.data>curr.next.data){
+                let temp = curr.data
+                curr.data = curr.next.data
+                curr.next.data = temp
+                swapped = true
+              }
+              curr = curr.next
+            }
+        } while (swapped);
+    }
+
+
+    print() {
+        if (this.front === null) {
             console.log("Queue is empty");
-        }else{
+        } else {
             let curr = this.front
             let elems = ""
-            while(curr!==null){
-                elems+=curr.data+" "
+            while (curr !== null) {
+                elems += curr.data + " "
                 curr = curr.next
             }
             console.log(elems);
@@ -52,10 +96,14 @@ class Queue {
 
 
 const queue = new Queue()
-queue.enqueue(10)
-queue.enqueue(20)
-queue.enqueue(30)
+// queue.enqueue(10)
+// queue.enqueue(20)
+// queue.enqueue(30)
+queue.enqueFormat([3, 2, -2, -6, 0, 7, 4])
 queue.print()
-queue.dequeue()
+// queue.dequeue()
+queue.reverse()
+queue.print()
+queue.sort()
 queue.print()
 
