d4104edefb43e781e11bd4ace894f3ee9dd18544
Navaneeth V <navaneethvinod18@gmail.com>
2024-03-16 22:48:40 +0530

    string

--- string/elementary.js
+++ string/elementary.js
@@ -3,9 +3,13 @@ let str = "apple"
 function rotateString(str, k) {
     k = k % str.length
     let arr = str.split('')
-    reverse(arr, 0, arr.length-1)
-    reverse(arr, 0, k-1)
-    reverse(arr, k, arr.length-1)
+    for(let i=0;i<k;i++){
+        let temp = arr[i]
+        for(let j=1;j<arr.length;j++){
+            arr[j] = arr[j+1]
+        }
+        arr[arr.length-1] = temp
+    }
     return arr.join('')
 }
 
@@ -19,7 +23,7 @@ function reverse(arr, start, end){
     }
 }
 
-console.log(rotateString(str, 2));
+console.log(rotateString(str, 1));
 
 
 function swapString(str, i, j){
