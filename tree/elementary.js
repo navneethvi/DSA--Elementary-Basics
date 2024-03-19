508d5172944bcd043b5390584f7c2b8bdfe937ab
Navaneeth V <navaneethvinod18@gmail.com>
2024-03-19 23:56:56 +0530

    play

--- tree/elementary.js
+++ tree/elementary.js
@@ -17,5 +17,5 @@ class BinarySearchTree {
 
 }
 
-// const bst = new BinarySearchTree()
-// console.log("Tree is empty : ",bst.isEmpty());
+const bst = new BinarySearchTree()
+console.log("Tree is empty : ",bst.isEmpty());
