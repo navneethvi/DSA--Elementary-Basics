dbd69e985307c9546eba7165a4fef9f729b874ae
Navaneeth V <navaneethvinod18@gmail.com>
2024-03-20 19:36:58 +0530

    play

--- tree/elementary.js
+++ tree/elementary.js
@@ -1,21 +1,21 @@
-// class Node {
-//     constructor(value){
-//         this.data = value
-//         this.right = null
-//         this.left = null
-//     }
-// }
+class Node {
+    constructor(value){
+        this.data = value
+        this.right = null
+        this.left = null
+    }
+}
 
-// class BinarySearchTree {
-//     constructor(){
-//         this.root = null
-//     }
+class BinarySearchTree {
+    constructor(){
+        this.root = null
+    }
 
-//     isEmpty(){
-//         return this.root === null
-//     }
+    isEmpty(){
+        return this.root === null
+    }
 
-// }
+}
 
-// const bst = new BinarySearchTree()
-// console.log("Tree is empty : ",bst.isEmpty());
+const bst = new BinarySearchTree()
+console.log("Tree is empty : ",bst.isEmpty());
