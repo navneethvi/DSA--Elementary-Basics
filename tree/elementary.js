418b0b2f0380c6fa70fe6426290012de7e576616
Navaneeth V <navaneethvinod18@gmail.com>
2024-03-22 10:39:59 +0530

    tree

--- tree/elementary.js
+++ tree/elementary.js
@@ -15,7 +15,35 @@ class BinarySearchTree {
         return this.root === null
     }
 
+    insert(value){
+        const newNode = new Node(value)
+        if(this.root === null){
+            this.root = newNode
+        }else{
+            this.insertNode(this.root, newNode)
+        }
+    }
+
+    insertNode(root, newNode){
+        if(newNode.data < root.data){
+            if(root.left === null){
+                root.left = newNode
+            }else{
+                this.insertNode(root.left, newNode)
+            }
+        }else{
+            if(root.right === null){
+                root.left = newNode
+            }else{
+                this.insertNode(root.right, newNode)
+            }
+        }
+    }
+
 }
 
 const bst = new BinarySearchTree()
+console.log("Tree is empty : ",bst.isEmpty());
+bst.insert(10)
+bst.insert(20)
 console.log("Tree is empty : ",bst.isEmpty());
